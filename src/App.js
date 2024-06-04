b648e0d12e6c747dec4bf1506d8d5f4bd6ac7790
gopisarvesh <gopi123abi@gmail.com>
2024-06-04 16:31:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+//import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
-function App() {
+export default function App() {
+  const [key, setKey] = useState(0);
+  const handleIncrement = () => {
+    setKey(parseInt(key) + 1);
+  };
+  const handleDecrement = () => {
+    setKey(parseInt(key) - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(key) => setKey(key)}
+        className="mb-3"
+      >
+        <Tab eventKey="0" title="Tab1">
+          <div>
+            Tab content for Tab1
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+            />
+            <br />
+            <input type="text" className="form-control" placeholder="Age" />
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Mobile No."
+            />
+            <br />
+            <button className="btn btn-primary" onClick={handleIncrement}>
+              Next
+            </button>
+          </div>
+        </Tab>
+        <Tab eventKey="1" title="Tab2">
+          Tab content for Tab2
+          <input type="text" className="form-control" placeholder="Username" />
+          <br />
+          <input type="text" className="form-control" placeholder="Age" />
+          <br />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Mobile No."
+          />
+          <br />
+          <button className="btn btn-secondary" onClick={handleDecrement}>
+            Previous
+          </button>
+          &nbsp;
+          <button className="btn btn-primary" onClick={handleIncrement}>
+            Next
+          </button>
+        </Tab>
+        <Tab eventKey="2" title="Tab3">
+          Tab content for Tab3
+          <input type="text" className="form-control" placeholder="Username" />
+          <br />
+          <input type="text" className="form-control" placeholder="Age" />
+          <br />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Mobile No."
+          />
+          <br />
+          <button className="btn btn-secondary" onClick={handleDecrement}>
+            Previous
+          </button>
+          &nbsp;
+          <button className="btn btn-primary">Next</button>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
-
-export default App;
